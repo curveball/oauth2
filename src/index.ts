40e3c3ea5d71db5e57ab579a969f4efe0af3f0b5
Evert Pot <me@evertpot.com>
2022-05-17 16:06:33 -0400

    Lets just provide the polyfill

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,21 @@
+/* eslint "@typescript-eslint/no-var-requires": 0 */
+
 import './definitions';
 export { default as default } from './middleware';
 export { AuthHelper, Principal } from './auth-helper';
 export { PrivilegeHelper } from './privilege-helper';
+
+if (!global.fetch) {
+  const nodeFetch = require('node-fetch');
+  global.fetch = nodeFetch;
+  global.Headers = nodeFetch.Headers;
+  global.Request = nodeFetch.Request;
+  global.Response = nodeFetch.Response;
+}
+
+// For Node 14.x and below
+if (global.btoa === undefined) {
+  global.btoa = input => {
+    return Buffer.from(input).toString('base64');
+  };
+}
