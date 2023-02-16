14b0d9248f473c6e6bc213ffee66e8797ca8373b
Evert Pot <me@evertpot.com>
2023-02-16 14:29:04 -0500

    Fix polyfill

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,21 @@
-/* eslint "@typescript-eslint/no-var-requires": 0 */
-
 import './definitions.js';
 export { default as default } from './middleware.js';
 export { AuthHelper, Principal } from './auth-helper.js';
 export { PrivilegeHelper } from './privilege-helper.js';
 
-if (!global.fetch) {
-  const nodeFetch = require('node-fetch');
-  global.fetch = nodeFetch;
-  global.Headers = nodeFetch.Headers;
-  global.Request = nodeFetch.Request;
-  global.Response = nodeFetch.Response;
+import fetch, {
+  Headers,
+  Request,
+  Response,
+} from 'node-fetch';
+
+if (!globalThis.fetch) {
+  // @ts-expect-error These types arent identical but they will do
+  globalThis.fetch = fetch;
+  // @ts-expect-error These types arent identical but they will do
+  globalThis.Headers = Headers;
+  // @ts-expect-error These types arent identical but they will do
+  globalThis.Request = Request;
+  // @ts-expect-error These types arent identical but they will do
+  globalThis.Response = Response;
 }
